92dede37fb3704ab26ccab65ca34ebdee6f6d443
andresfloresloza <luisandresfloresloza@gmail.com>
2023-06-14 01:07:41 -0400

    Corrección imagen inicial-actualizacion 3.5

--- backend/settings.py
+++ backend/settings.py
@@ -136,7 +136,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = '/media/'
-MEDIA_URL = '/media/'
 
 if not DEBUG:
     STATIC_ROOT = os.path.join(BASE_DIR, 'media')
