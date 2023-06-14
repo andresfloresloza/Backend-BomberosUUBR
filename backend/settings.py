18968fa5bc8b8e055e61ae786b1b7dcfaac8bd50
andresfloresloza <luisandresfloresloza@gmail.com>
2023-06-13 23:24:37 -0400

    Corrección image

--- backend/settings.py
+++ backend/settings.py
@@ -139,6 +139,7 @@ STATIC_URL = '/media/'
 
 if not DEBUG:
     STATIC_ROOT = os.path.join(BASE_DIR, 'media')
+    STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
