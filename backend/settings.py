1ab40e7cb877d15ef2f1f3f861203a2ffbd5310a
andresfloresloza <luisandresfloresloza@gmail.com>
2023-06-13 22:15:51 -0400

    implementacion render 2.2

--- backend/settings.py
+++ backend/settings.py
@@ -157,6 +157,7 @@ SIMPLE_JWT = {
     "ACCESS_TOKEN_LIFETIME": timedelta(minutes=1000),
     "REFRESH_TOKEN_LIFETIME": timedelta(days=1)
 }
-
+"""
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
+"""
