4a6195e3b646e45652b7a654f444233038b1b379
andresfloresloza <luisandresfloresloza@gmail.com>
2023-06-14 01:20:39 -0400

    Corrección imagen inicial-actualizacion 3.6

--- backend/settings.py
+++ backend/settings.py
@@ -135,10 +135,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-MEDIA_URL = '/media/'
+STATIC_URL = '/media/'
 
 if not DEBUG:
-    MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+    STATIC_ROOT = os.path.join(BASE_DIR, 'media')
     STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Default primary key field type
