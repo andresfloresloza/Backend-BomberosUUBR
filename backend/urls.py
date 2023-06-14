f5a337891197a6daf39445afd04a8c9750dab5f2
andresfloresloza <luisandresfloresloza@gmail.com>
2023-06-14 01:11:49 -0400

    Corrección imagen inicial-actualizacion 3.6

--- backend/urls.py
+++ backend/urls.py
@@ -19,7 +19,7 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 from rest_framework_simplejwt.views import TokenRefreshView, TokenVerifyView
-from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+
 from authentication.api import MyTokenObtainPairView
 
 urlpatterns = [
@@ -29,6 +29,4 @@ urlpatterns = [
                   path('login/', MyTokenObtainPairView.as_view(), name='token_obtain_pair'),
                   path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
                   path('token/verify/', TokenVerifyView.as_view(), name='token_verify'),
-              ]
-
-urlpatterns += staticfiles_urlpatterns()
+              ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
