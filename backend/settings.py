3d3fdb490210b3f2611a6aafbc8b87c9c4182589
andresfloresloza <luisandresfloresloza@gmail.com>
2023-06-13 22:33:05 -0400

    creacion superuser 2

--- backend/settings.py
+++ backend/settings.py
@@ -138,9 +138,9 @@ USE_TZ = True
 STATIC_URL = 'static/'
 
 if not DEBUG:
-    STATIC_ROOT = os.path.join(BASE_DIR, 'media')
+    MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
     MEDIA_URL = '/media/'
-    STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+    MEDIAFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
--- build.sh
+++ build.sh
@@ -8,6 +8,7 @@ python manage.py collectstatic --no-input
 python manage.py makemigrations
 python manage.py migrate
 
+
 echo "Creating superuser..."
 python manage.py shell << END
 from django.contrib.auth import get_user_model
@@ -16,5 +17,4 @@ User = get_user_model()
 if not User.objects.filter(username='luisandresfloresloza@gmail.com').exists():
     User.objects.create_superuser('luisandresfloresloza@gmail.com', 'luisandresfloresloza@gmail.com', '76680886')
 END
-
 echo "Superuser created successfully!"
