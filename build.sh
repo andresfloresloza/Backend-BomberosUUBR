220ed38a3dc12c89950ffc56325c95d478862af1
andresfloresloza <luisandresfloresloza@gmail.com>
2023-06-13 22:27:54 -0400

    creacion superuser 1

--- build.sh
+++ build.sh
@@ -7,4 +7,14 @@ pip install -r requirements.txt
 python manage.py collectstatic --no-input
 python manage.py makemigrations
 python manage.py migrate
-python manage.py createsuperuser
+
+echo "Creating superuser..."
+python manage.py shell << END
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
+if not User.objects.filter(username='luisandresfloresloza@gmail.com').exists():
+    User.objects.create_superuser('luisandresfloresloza@gmail.com', 'luisandresfloresloza@gmail.com', '76680886')
+END
+
+echo "Superuser created successfully!"
