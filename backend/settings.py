9ed46e4e921e82aa0c659d4c2e1b8959304c7d3f
andresfloresloza <luisandresfloresloza@gmail.com>
2023-06-13 22:35:12 -0400

    creacion superuser 3

--- backend/settings.py
+++ backend/settings.py
@@ -140,7 +140,6 @@ STATIC_URL = 'static/'
 if not DEBUG:
     MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
     MEDIA_URL = '/media/'
-    MEDIAFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
