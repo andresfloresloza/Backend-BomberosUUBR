111d59c3a0ee29d023d287b7e5bd6202651a8c13
andresfloresloza <luisandresfloresloza@gmail.com>
2023-06-14 01:54:02 -0400

    Corrección imagen inicial-actualizacion 3.10

--- backend/urls.py
+++ backend/urls.py
@@ -29,4 +29,4 @@ urlpatterns = [
                   path('login/', MyTokenObtainPairView.as_view(), name='token_obtain_pair'),
                   path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
                   path('token/verify/', TokenVerifyView.as_view(), name='token_verify'),
-              ]
+              ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
