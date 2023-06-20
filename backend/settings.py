b11dd015ce5ee608dd6ae2115f9374fb2988b973
andresfloresloza <luisandresfloresloza@gmail.com>
2023-06-19 20:40:10 -0400

    Comando Imagenes

--- backend/settings.py
+++ backend/settings.py
@@ -135,11 +135,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = '/media/'
+MEDIA_URL = '/media/'
 
 if not DEBUG:
-    STATIC_ROOT = os.path.join(BASE_DIR, 'media')
-    STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+    MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
