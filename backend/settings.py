3824483c77f98ccb459bb85b73aecb9cf4d7f169
andresfloresloza <luisandresfloresloza@gmail.com>
2023-06-28 12:18:52 -0400

    Correccion imagen

--- backend/settings.py
+++ backend/settings.py
@@ -134,12 +134,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
-
-STATIC_URL = '/media/'
-
-if not DEBUG:
-    STATIC_ROOT = os.path.join(BASE_DIR, 'media')
-    STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATIC_URL = '/static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
@@ -156,3 +151,6 @@ SIMPLE_JWT = {
     "ACCESS_TOKEN_LIFETIME": timedelta(minutes=1000),
     "REFRESH_TOKEN_LIFETIME": timedelta(days=1)
 }
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA = '/media/'
